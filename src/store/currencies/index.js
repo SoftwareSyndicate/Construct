4d4211c6c6372aff5b6bdd903962652b06270b37
yintothayang <yintothayang@gmail.com>
2017-10-01 15:16:11 +0900

    config

--- src/store/currencies/index.js
+++ src/store/currencies/index.js
@@ -3,7 +3,7 @@ import * as actions from './actions'
 import mutations from './mutations'
 
 const state = {
-  limit: 10,
+  limit: 100,
   rate: 1000 * 30,
   currencies: [],
   sorted_currencies: [],
