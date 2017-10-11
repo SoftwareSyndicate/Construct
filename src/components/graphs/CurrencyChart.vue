65bbb5aa339f72b6784e5d69740ac5cb3948f978
yintothayang <yintothayang@gmail.com>
2017-10-11 20:26:59 +0900

    good, but with focus bug

--- src/components/graphs/CurrencyChart.vue
+++ src/components/graphs/CurrencyChart.vue
@@ -33,15 +33,23 @@ export default {
         x: {},
         y: {},
         z: {},
-      }
+      },
+      series: {
+
+      },
     }
   },
   watch: {
     chartData: {
       handler: function(newData, oldData){
         if(this.chartData.length && this.svg){
-          this.draw(this.chartData)
-            // this.fuckMe(this.chartData)
+          this.formatedData = this.chartData.map(d => {
+            return {
+              date: moment.unix(d.time),
+              close: parseFloat(d.close),
+            }
+          })
+          this.draw(this.formatedData)
         }
       },
       immediate: true,
@@ -52,209 +60,6 @@ export default {
     this.initChart()
   },
   methods: {
-    fuckMe(data){
-      var margin = {top: 20, right: 20, bottom: 50, left: 50}
-      var width = this.$refs.chart_container.clientWidth - margin.left - margin.right
-      var height = this.$refs.chart_container.clientHeight - margin.top - margin.bottom
-
-      var svg = d3.select('#chart')
-        .append("svg:svg")
-        .attr('width', width + margin.left + margin.right)
-        .attr('height', height + margin.top + margin.bottom)
-        .append("svg:g")
-	      .attr("id","group")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      var parseTime = d3.timeParse("%d/%m/%Y %H:%M")
-	    var bisectDate = d3.bisector(function(d) { return d.dt; }).left
-	    var formatValue = d3.format(",.2f")
-      var formatCurrency = function(d) { return "£" + formatValue(d); }
-	    
-      var x = d3.scaleTime()
-		      .range([0, width]);
-
-      var y = d3.scaleLinear()
-		      .range([height, 0]);
-
-      var priceSeries = d3.line()
-	        .defined(function(d) { return d.price != 0; })
-	        .x(function(d) { return x(d.dt); })
-	        .y(function(d) { return y(d.price); });
-
-      var focus = svg.append("g") 
-			    .style("display", "none")	
-
-	    // format the data
-	    data.forEach(function(d,i) {
-        // date: moment.unix(d.time),
-        //   close: parseFloat(d.close),
-		    d.dt = moment.unix(d.time)
-		    d.price = +d.close;
-	    });
-
-
-	    // Scale the range of the data
-	    console.log("Date range: ", d3.extent(data, function(d) { return d.dt; }));
-	    console.log("Price range: ", d3.extent(data, function(d) { return +d.price; }));	
-	    x.domain(d3.extent(data, function(d) { return d.dt; }));
-	    y.domain(d3.extent(data, function(d) { return d.price; }));
-	    
-	    var zoom = d3.zoom()
-          .scaleExtent([0.75, 15000])
-          .translateExtent([[-100000, -100000], [100000, 100000]])
-          .on("zoom", zoomed);	
-	    
-	    var xAxis = d3.axisBottom(x)
-		      .ticks((width + 2) / (height + 2) * 5)
-		      .tickSize(-height)
-		      .tickPadding(10);
-		  
-	    var yAxis = d3.axisRight(y)
-		      .ticks(5)
-		      .tickSize(width)
-		      .tickPadding(- 20 - width);
-	    
-	    svg.append("text")             
-	      .attr("transform",
-			        "translate(" + (width/2) + " ," + (height + margin.top + 20) + ")")
-	      .style("font-size","12px")
-	      .style("font-family", "sans-serif")
-	      .style("text-anchor", "middle")
-	      .text("Date/Time");
-	    
-	    svg.append("text")
-		    .attr("transform", "rotate(-90)")
-		    .attr("y", 0 - margin.left/1.2)
-		    .attr("x",0 - (height / 2))
-		    .attr("dy", "1em")
-		    .style("font-size","12px")
-		    .style("text-anchor", "middle")
-		    .text("Price");
-
-	    var view = svg.append("rect")
-		      .attr("class", "view")
-		      .attr("x", 0.5)
-		      .attr("y", 0.5)
-		      .attr("width", width - 1)
-		      .attr("height", height - 1);
-
-	    var gX = svg.append("g")
-		      .attr("class", "axis axis--x")
-		      .attr("transform", "translate(0," + height + ")")
-		      .call(xAxis);
-
-	    var gY = svg.append("g")
-		      .attr("class", "axis axis--y")
-		      .call(yAxis);
-		  
-	    svg.append("defs").append("clipPath")
-		    .attr("id", "clip")
-	      .append("rect")
-		    .attr("width", width)
-		    .attr("height", height);
-		  
-	    var chartBody = svg.append("g")
-		      .attr("clip-path", "url(#clip)");
-		  
-	    focus.append("circle")
-		    .attr("class", "y")
-		    .style("fill", "none")
-		    .style("stroke", "steelblue")
-		    .attr("r", 4)
-		    .attr("clip-path", "url(#clip)");	
-		  
-	    focus.append("text")
-		    .attr("dy", ".35em")
-  		  .attr("clip-path", "url(#clip)");
-		  
-	    chartBody.append("svg:path")
-		    .data([data])
-		    .attr("class", "line")
-		    .attr("d", priceSeries);
-		  
-	    svg.append("rect")
-		    .attr("id","rect")
-		    .attr("width", width)
-		    .attr("height", height)
-        .style("fill", "none")
-        .style("pointer-events", "all")
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mouseout", function() { focus.style("display", "none"); })
-        .on("mousemove", mousemove);
-		  
-	    svg.append("g")
-		    .attr("transform", "translate(0," + height + ")")
-		    .call(d3.axisBottom(x).ticks(0));
-		  
-	    svg.append("g")
-	      .call(d3.axisLeft(y).ticks(0));
-	    
-	    d3.select("#rect").call(zoom);
-	    
-	    d3.select("button").on("click", resetted);
-	    
-	    function mouseDate(scale) {
-		    var g = d3.select("#group")._groups[0][0]
-		    var x0 = scale.invert(d3.mouse(g)[0])
-		    var i = bisectDate(data, x0, 1)
-		    var d0 = data[i - 1]
-		    var d1 = data[i]
-
-        if(!d1){
-          return d0
-        }
-		    var d = x0 - d0.date > d1.date - x0 ? d1 : d0
-	      return d;
-	    }
-	    
-	    function mousemove() {
-		    var transform = d3.zoomTransform(this);
-		    var xt = transform.rescaleX(x), yt = transform.rescaleY(y);
-		    var d = mouseDate(xt);
-		    focus.select("circle.y")
-			    .attr('cx', function() {
-						return transform.applyX(x(d.dt));
-					})
-			    .attr('cy', function() {
-						return transform.applyY(y(d.price));
-					});
-		    focus.select("text")
-			    .text(formatCurrency(d.price))
-			    .attr('x', function() {
-						return transform.applyX(x(d.dt))+10;
-			    })
-			    .attr('y', function() {
-						return transform.applyY(y(d.price));
-			    });
-
-	    }
-	    
-	    function zoomed() {
-	      
-	      gX.call(xAxis.scale(d3.event.transform.rescaleX(x)));
-	      gY.call(yAxis.scale(d3.event.transform.rescaleY(y)));
-	      var t = d3.event.transform, xt = t.rescaleX(x), yt = t.rescaleY(y)
-	      svg.select(".line")
-		      .attr("d",priceSeries.x(function(d) { return xt(d.dt);})
-							  .y(function(d) { return yt(d.price);}));
-		    // var d = mouseDate(x);
-		    // focus.select("circle.y")
-			  //   .classed("zoomed", true)
-			  //   .attr("id","one")
-			  //   .attr('cx', function() {return t.applyX(x(d.dt)); })
-			  //   .attr('cy', function() {return t.applyY(y(d.price)); });				
-		    // focus.select("text")
-			  //   .text(formatCurrency(d.price))
-			  //   .attr('x', function() { return t.applyX(x(d.dt))+10;})
-			  //   .attr('y',function() {return t.applyY(y(d.price)); });
-	    }
-	    
-	    function resetted() {
-	      d3.select("#rect").transition()
-		      .duration(750)
-		      .call(zoom.transform, d3.zoomIdentity);
-	    }
-    },
     initChart(){
       this.margin = {top: 20, right: 20, bottom: 50, left: 50}
       this.width = this.$refs.chart_container.clientWidth - this.margin.left - this.margin.right
@@ -274,9 +79,9 @@ export default {
       // this.generateScales()
       
       // Close Line  -> TODO Dynamically Generate
-      this.closeLine = d3.line()
-        .x((d) => { return this.scales['x']['bottom'](d.date)})
-        .y((d) => { return this.scales['y']['left'](d.close)})
+      // this.closeLine = d3.line()
+      //   .x((d) => { return this.scales['x']['bottom'](d.date)})
+      //   .y((d) => { return this.scales['y']['left'](d.close)})
 
     },
     generateScales(){
@@ -287,61 +92,47 @@ export default {
       // this.scales['y']['right'] = d3.scaleLinear().rangeRound([this.height, 0])
     },
     draw(data){
-      data = data.map(d => {
-        return {
-          dt: moment.unix(d.time),
-          price: parseFloat(d.close),
-        }
-      })
 
+      // Format Data
       // Set scales domains to min and max of data set
       // this.scales['x']['bottom'].domain(d3.extent(data, (d) => { return d.date }))
       // this.scales['y']['left'].domain(d3.extent(data, (d) => { return d.close }))
 
-	    var bisectDate = d3.bisector(function(d) { return d.dt; }).left
-	    var formatValue = d3.format(",.2f")
-      var formatCurrency = function(d) { return "£" + formatValue(d); }
-	    
-      var x = d3.scaleTime()
-		      .range([0, this.width]);
+	    this.bisectDate = d3.bisector((d) => { return d.date }).left
+	    this.formatValue = d3.format(",.2f")
+      this.formatCurrency = (d) =>  { return "$" + this.formatValue(d) }
 
-      var y = d3.scaleLinear()
-		      .range([this.height, 0]);
 
-      var priceSeries = d3.line()
-	        .defined(function(d) { return d.price != 0; })
-	        .x(function(d) { return x(d.dt); })
-	        .y(function(d) { return y(d.price); });
+      // Init Scales
+      this.scales.x.bottom = d3.scaleTime()
+		      .range([0, this.width])
 
-      var focus = this.svg.append("g") 
+      this.scales.y.left = d3.scaleLinear()
+		      .range([this.height, 0])
+
+      this.series.close = d3.line()
+	        .defined((d) => { return d.price != 0 })
+	        .x((d) => { return this.scales.x.bottom(d.date); })
+	        .y((d) =>  { return this.scales.y.left(d.close); });
+
+      this.focus = this.svg.append("g") 
 			    .style("display", "none")	
 
-	    // // format the data
-	    // data.forEach(function(d,i) {
-      //   // date: moment.unix(d.time),
-      //   //   close: parseFloat(d.close),
-		  //   d.dt = moment.unix(d.time)
-		  //   d.price = +d.close;
-	    // });
-
-
-	    // Scale the range of the data
-	    console.log("Date range: ", d3.extent(data, function(d) { return d.dt; }));
-	    console.log("Price range: ", d3.extent(data, function(d) { return +d.price; }));	
-	    x.domain(d3.extent(data, function(d) { return d.dt; }));
-	    y.domain(d3.extent(data, function(d) { return d.price; }));
+	    // Scale the range of the data   TODO  - Make dynamic
+	    this.scales.x.bottom.domain(d3.extent(data, (d) => { return d.date }))
+	    this.scales.y.left.domain(d3.extent(data, (d) => { return d.close }))
 	    
-	    var zoom = d3.zoom()
+	    this.zoom = d3.zoom()
           .scaleExtent([0.75, 15000])
           .translateExtent([[-100000, -100000], [100000, 100000]])
-          .on("zoom", zoomed);	
+          .on("zoom", this.zoomed.bind(this));	
 	    
-	    var xAxis = d3.axisBottom(x)
+	    this.axes.x.bottom = d3.axisBottom(this.scales.x.bottom)
 		      .ticks((this.width + 2) / (this.height + 2) * 5)
 		      .tickSize(-this.height)
 		      .tickPadding(10);
 		  
-	    var yAxis = d3.axisRight(y)
+	    this.axes.y.left = d3.axisRight(this.scales.y.left)
 		      .ticks(5)
 		      .tickSize(this.width)
 		      .tickPadding(- 20 - this.width);
@@ -363,21 +154,21 @@ export default {
 		    .style("text-anchor", "middle")
 		    .text("Price");
 
-	    var view = this.svg.append("rect")
+	    this.view = this.svg.append("rect")
 		      .attr("class", "view")
 		      .attr("x", 0.5)
 		      .attr("y", 0.5)
 		      .attr("width", this.width - 1)
 		      .attr("height", this.height - 1);
 
-	    var gX = this.svg.append("g")
+	    this.axes_elements.x.bottom = this.svg.append("g")
 		      .attr("class", "axis axis--x")
 		      .attr("transform", "translate(0," + this.height + ")")
-		      .call(xAxis);
+		      .call(this.axes.x.bottom);
 
-	    var gY = this.svg.append("g")
+	    this.axes_elements.y.left = this.svg.append("g")
 		      .attr("class", "axis axis--y")
-		      .call(yAxis);
+		      .call(this.axes.y.left);
 		  
 	    this.svg.append("defs").append("clipPath")
 		    .attr("id", "clip")
@@ -385,24 +176,24 @@ export default {
 		    .attr("width", this.width)
 		    .attr("height", this.height);
 		  
-	    var chartBody = this.svg.append("g")
+	    this.chartBody = this.svg.append("g")
 		      .attr("clip-path", "url(#clip)");
 		  
-	    focus.append("circle")
+	    this.focus.append("circle")
 		    .attr("class", "y")
 		    .style("fill", "none")
 		    .style("stroke", "steelblue")
 		    .attr("r", 4)
 		    .attr("clip-path", "url(#clip)");	
 		  
-	    focus.append("text")
+	    this.focus.append("text")
 		    .attr("dy", ".35em")
   		  .attr("clip-path", "url(#clip)");
 		  
-	    chartBody.append("svg:path")
+	    this.chartBody.append("svg:path")
 		    .data([data])
 		    .attr("class", "line")
-		    .attr("d", priceSeries);
+		    .attr("d", this.series.close);
 		  
 	    this.svg.append("rect")
 		    .attr("id","rect")
@@ -410,85 +201,93 @@ export default {
 		    .attr("height", this.height)
         .style("fill", "none")
         .style("pointer-events", "all")
-        .on("mouseover", function() { focus.style("display", null); })
-        .on("mouseout", function() { focus.style("display", "none"); })
-        .on("mousemove", mousemove);
-		  
+        .on("mouseover", () => { this.focus.style("display", null); })
+        .on("mouseout", () => { this.focus.style("display", "none"); })
+        .on("mousemove", this.mousemove.bind(this));
+
 	    this.svg.append("g")
 		    .attr("transform", "translate(0," + this.height + ")")
-		    .call(d3.axisBottom(x).ticks(0));
+		    .call(d3.axisBottom(this.scales.x.bottom).ticks(0));
 		  
 	    this.svg.append("g")
-	      .call(d3.axisLeft(y).ticks(0));
+	      .call(d3.axisLeft(this.scales.y.left).ticks(0));
 	    
-	    d3.select("#rect").call(zoom);
+	    d3.select("#rect").call(this.zoom.bind(this));
 	    
-	    d3.select("button").on("click", resetted);
-	    
-	    function mouseDate(scale) {
-		    var g = d3.select("#group")._groups[0][0]
-		    var x0 = scale.invert(d3.mouse(g)[0])
-		    var i = bisectDate(data, x0, 1)
-		    var d0 = data[i - 1]
-		    var d1 = data[i]
+	    // d3.select("button").on("click", this.resetted.bind(this));
+	  },
+    
+	  mouseDate(scale) {
+		  var g = d3.select("#group")._groups[0][0]
+		  var x0 = scale.invert(d3.mouse(g)[0])
+		  var i = this.bisectDate(this.formatedData, x0, 1)
+		  var d0 = this.formatedData[i - 1]
+		  var d1 = this.formatedData[i]
 
-        if(!d1){
-          return d0
-        }
-		    var d = x0 - d0.date > d1.date - x0 ? d1 : d0
-	      return d;
-	    }
-	    
-	    function mousemove() {
-		    var transform = d3.zoomTransform(this);
-		    var xt = transform.rescaleX(x), yt = transform.rescaleY(y);
-		    var d = mouseDate(xt);
-		    focus.select("circle.y")
-			    .attr('cx', function() {
-						return transform.applyX(x(d.dt));
-					})
-			    .attr('cy', function() {
-						return transform.applyY(y(d.price));
-					});
-		    focus.select("text")
-			    .text(formatCurrency(d.price))
-			    .attr('x', function() {
-						return transform.applyX(x(d.dt))+10;
-			    })
-			    .attr('y', function() {
-						return transform.applyY(y(d.price));
-			    });
+      // TODO DEBUG THIS
 
-	    }
+      if(!d1){
+        return d0
+      }
+		  var d = x0 - d0.date > d1.date - x0 ? d1 : d0
+	    return d;
+	  },
+	  
+	  mousemove() {
+		  var transform = d3.zoomTransform(this);
+		  let xt = transform.rescaleX(this.scales.x.bottom)
+      let yt = transform.rescaleY(this.scales.y.left)
 
-	    var svg = this.svg
-	    function zoomed() {
-	      gX.call(xAxis.scale(d3.event.transform.rescaleX(x)));
-	      gY.call(yAxis.scale(d3.event.transform.rescaleY(y)));
-	      var t = d3.event.transform, xt = t.rescaleX(x), yt = t.rescaleY(y)
-	      svg.select(".line")
-		      .attr("d",priceSeries.x(function(d) { return xt(d.dt);})
-							  .y(function(d) { return yt(d.price);}));
-		    // var d = mouseDate(x);
-		    // focus.select("circle.y")
-			  //   .classed("zoomed", true)
-			  //   .attr("id","one")
-			  //   .attr('cx', function() {return t.applyX(x(d.dt)); })
-			  //   .attr('cy', function() {return t.applyY(y(d.price)); });				
-		    // focus.select("text")
-			  //   .text(formatCurrency(d.price))
-			  //   .attr('x', function() { return t.applyX(x(d.dt))+10;})
-			  //   .attr('y',function() {return t.applyY(y(d.price)); });
-	    }
-	    
-	    function resetted() {
-	      d3.select("#rect").transition()
-		      .duration(750)
-		      .call(zoom.transform, d3.zoomIdentity);
-	    }
+		  var d = this.mouseDate(xt)
       
-    }
-  },
+		  this.focus.select("circle.y")
+			  .attr('cx', () => {
+					return transform.applyX(this.scales.x.bottom(d.date));
+				})
+			  .attr('cy', () => {
+					return transform.applyY(this.scales.y.left(d.close));
+				})
+      
+		  this.focus.select("text")
+			  .text(this.formatCurrency(d.close))
+			  .attr('x', () => {
+					return transform.applyX(this.scales.x.bottom(d.date))+10;
+			  })
+			  .attr('y', () => {
+					return transform.applyY(this.scales.y.left(d.close));
+			  })
+	  },
+
+	  zoomed() {
+	    this.axes_elements.x.bottom.call(this.axes.x.bottom.scale(d3.event.transform.rescaleX(this.scales.x.bottom)));
+	    this.axes_elements.y.left.call(this.axes.y.left.scale(d3.event.transform.rescaleY(this.scales.y.left)));
+	    let t = d3.event.transform
+      let xt = t.rescaleX(this.scales.x.bottom)
+      let yt = t.rescaleY(this.scales.y.left)
+      
+	    this.svg.select(".line")
+		    .attr("d", this.series.close
+              .x((d) => { return xt(d.date)})
+							.y((d) => { return yt(d.close)}))
+
+		  var d = this.mouseDate(this.scales.x.bottom)
+		  this.focus.select("circle.y")
+			  .classed("zoomed", true)
+			  .attr("id","one")
+			  .attr('cx', () => {return t.applyX(this.scales.x.bottom(d.date)) })
+			  .attr('cy', () => {return t.applyY(this.scales.y.left(d.close)) })
+		  this.focus.select("text")
+			  .text(this.formatCurrency(d.close))
+			  .attr('x', () => { return t.applyX(this.scales.x.bottom(d.date))+10})
+			  .attr('y', () => { return t.applyY(this.scales.y.left(d.close)) })
+	  },
+	  
+	  resetted() {
+	    d3.select("#rect").transition()
+		    .duration(750)
+		    .call(this.zoom.transform, d3.zoomIdentity)
+	  }
+  }
 }
 </script>
 
